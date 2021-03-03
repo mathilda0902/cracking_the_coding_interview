b6b7bcf9463a2bc0e14328b2ab59bd941efdac01
mathilda0902 <mathilda_sorciere@yahoo.com>
2021-03-03 14:09:16 -0800

    palindrome linked list

--- cha2_linked_list.py
+++ cha2_linked_list.py
@@ -58,13 +58,77 @@ class LinkedList:
             n.val = n.next.val
             n.next = n.next.next
 
+    # reverse linked list
+    def reverse(self):
+        prev = None
+        current = self.head
+        while(current != None):
+            next_tmp = current.next
+            current.next = prev
+            prev = current
+            current = next_tmp
+
+        self.head = prev
+        return self
+
+    # palindrome
+    def is_palindrome1(self) -> bool:
+        # approach 1: reverse and compare
+        reversed = self.reverse()
+        for node in reversed:
+            print(node.val)
+        node_A = self.head
+        node_B = reversed.head
+
+        while (node_A != None and node_B != None):
+            if node_A == node_B:
+                node_A = node_A.next
+                node_B = node_B.next
+            else:
+                return False
+            
+        return True
+
+
+    def is_palindrome2(self) -> bool:
+    # approach 2: iterative, fast slow pointers
+        fast = slow = self.head
+        stack = []
+        
+        while (fast != None and fast.next != None):
+            stack.append(slow.val)
+            fast = fast.next.next
+            slow = slow.next
+            
+        if fast != None:
+            slow = slow.next
+            
+        while (slow != None):
+            top = stack.pop()
+            if slow.val != top:
+                return False
+            slow = slow.next
+        
+        return True
+
+def is_palindrome3():
+    # approach 3: recrusive
+    pass
+
 
 if __name__ == '__main__':
     llist = LinkedList(nodes=[0,1,2,3,2,3,3,3,5])
+    llist = LinkedList(nodes=[1,2,3,2,1])
+    llist = LinkedList(nodes=[1,2,2,1])
 
     # llist.remove_dups(llist.head)
     # for node in llist:
     #     print(node.val)
 
-    print(llist.find_k_last(llist.head, 7))
+    # print(llist.find_k_last(llist.head, 7))
+    reversed_llist = llist.reverse()
+   
+    # for node in reversed_llist:
+    #     print(node.val)
 
+    print(llist.is_palindrome2())
